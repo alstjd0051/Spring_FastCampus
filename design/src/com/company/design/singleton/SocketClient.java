51a7cd6f650b9b56a9be76a5b027d6244fb0250a
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-05-18 02:26:10 +0900

    싱글톤 패턴

---
+++ design/src/com/company/design/singleton/SocketClient.java
@@ -0,0 +1,20 @@
+package com.company.design.singleton;
+
+public class SocketClient {
+
+    private static SocketClient socketClient = null;
+
+    public SocketClient() {
+
+    }
+    public static SocketClient getInstance() {
+
+        if(socketClient.socketClient == null) {
+            socketClient = new SocketClient();
+        }
+        return socketClient;
+    }
+    public void connect() {
+        System.out.println("connect");
+    }
+}
