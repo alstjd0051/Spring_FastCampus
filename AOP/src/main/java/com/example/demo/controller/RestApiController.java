745de940fb7a4920f147364cd549113a878f1eaa
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-05-28 22:16:15 +0900

    no commit message

---
+++ AOP/src/main/java/com/example/demo/controller/RestApiController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+	@GetMapping("/get/{id}")
+	public void get(@PathVariable Long id, @RequestParam String name) {
+		System.out.println("get method");
+		System.out.println("get method : " + id);
+		System.out.println("get method : " + name);
+	}
+
+	@PostMapping("/post")
+	public void post(@RequestBody) {
+
+	}
+}
